90cdee64255e2e0b1d0a7f0ef7c4fe3353b475a0
Piotr Karpala <piotrkarpala@microsoft.com>
2024-04-05 14:43:49 -0400

    composite action (#2)
    * composite action

---
+++ clean.py
@@ -0,0 +1,12 @@
+# /app/clean.py
+
+"""This is a module docstring."""
+
+
+def hello_world():
+    """This function prints 'Hello, world!'."""
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    hello_world()
