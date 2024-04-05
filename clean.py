8aee7cf2aa9659e3d2a29af0cd9e40092f1d0381
Piotr Karpala <piotrkarpala@microsoft.com>
2024-04-05 14:30:56 -0400

    linting

--- clean.py
+++ clean.py
@@ -2,9 +2,11 @@
 
 """This is a module docstring."""
 
+
 def hello_world():
     """This function prints 'Hello, world!'."""
     print("Hello, world!")
+
 
 if __name__ == "__main__":
     hello_world()
